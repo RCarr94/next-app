c8ab1bb35cece2a143da2e765c176caeb06cdb44
Ryan Carr <ryan.g.carr@hotmail.com>
2023-11-12 21:35:17 -0500

    layout basics setup

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+import AddToCart from './AddToCart';
+
+export default function ProductCard() {
+  return (
+    <>
+      <div>
+        <AddToCart />
+      </div>
+    </>
+  );
+}
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-export default function UsersPage() {
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export default async function UsersPage() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', { next: { revalidate: 30 } }); // switch to cache: 'no-store for production'
+  const users: User[] = await res.json();
+
   return (
-    <div>UsersPage</div>
-  )
+    <>
+      <h1>Users</h1>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
