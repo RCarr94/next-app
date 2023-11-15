38c539e9a310d5db017d7c3e48e1754070197c75
Ryan Carr <ryan.g.carr@hotmail.com>
2023-11-14 20:16:25 -0500

    update the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,27 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { AiFillBug } from 'react-icons/ai';
 
 export default function NavBar() {
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' },
+  ];
+
   return (
-    <nav>
-      <Link href="/">Logo</Link>
-      <ul>
-        <li><Link href="/">Dashboard</Link></li>
-        <li><Link href="/issues">Issues</Link></li>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map(link => 
+          <Link 
+          key={link.href} 
+          className="text-zinc-500 hover:text-zinc-800 transition-colors" 
+          href={link.href}>
+            {link.label}
+          </Link>
+        )}
       </ul>
     </nav>
-  )
+  );
 }
